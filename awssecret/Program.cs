6a8493126898f723d0d2ee7d99c7ddd5f664dd95
jaspal <jas@gmail.com>
2023-07-03 21:40:52 +0300

    Add project files.

---
+++ awssecret/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+namespace awssecret
+{
+    
+
+   class Program
+    {
+         static async Task GetSecret()
+        {
+            string secretName = "susername1";
+            string region = "us-east-1";
+
+            IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+
+            GetSecretValueRequest request = new GetSecretValueRequest
+            {
+                SecretId = secretName,
+                VersionStage = "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified.
+            };
+
+            GetSecretValueResponse response;
+
+            try
+            {
+                response = await client.GetSecretValueAsync(request);
+            }
+            catch (Exception e)
+            {
+                // For a list of the exceptions thrown, see
+                // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+                throw e;
+            }
+
+            string secret = response.SecretString;
+            Console.WriteLine(secret);
+
+            // Your code goes here
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            await GetSecret();
+        }
+    }
+
+
+
+}
